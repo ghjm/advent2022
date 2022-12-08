95f990e0ae46596c4050ccdc2879994b8c1193a5
Graham Mainwaring <graham@mhn.org>
2022-12-07 19:58:54 -0500

    Day 4

---
+++ cmd/04/p1.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ghjm/advent2022/pkg/utils"
+	"os"
+)
+
+func run() error {
+	matches, err := utils.OpenAndReadAllRegex("input4.txt", `^(\d+)-(\d+),(\d+)-(\d+)$`, true)
+	if err != nil {
+		return err
+	}
+	part1Count := 0
+	part2Count := 0
+	for _, m := range matches {
+		mi, err := utils.StringsToInts(m, 1, 2, 3, 4)
+		if err != nil {
+			return err
+		}
+		e1min, e1max, e2min, e2max := mi[0], mi[1], mi[2], mi[3]
+		if e1min > e2min {
+			tmpMin := e1min
+			tmpMax := e1max
+			e1min = e2min
+			e1max = e2max
+			e2min = tmpMin
+			e2max = tmpMax
+		}
+		if (e2min >= e1min && e2max <= e1max) || (e1min >= e2min && e1max <= e2max) {
+			part1Count += 1
+		}
+		if e2min <= e1max {
+			part2Count += 1
+		}
+	}
+	fmt.Printf("Part 1: %d\n", part1Count)
+	fmt.Printf("Part 2: %d\n", part2Count)
+	return nil
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+}
--- pkg/utils/read_input.go
+++ pkg/utils/read_input.go
@@ -1,78 +1,115 @@
 package utils
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "os"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strconv"
 )
 
 type InputFileReader interface {
-    io.ReadCloser
-    ReadLine() (line []byte, isPrefix bool, err error)
-    ReadAll(callback func(string) error) error
+	io.ReadCloser
+	ReadLine() (line []byte, isPrefix bool, err error)
+	ReadAll(callback func(string) error) error
 }
 
 type inputFileReader struct {
-    file *os.File
-    bufreader *bufio.Reader
+	file      *os.File
+	bufreader *bufio.Reader
 }
 
 func OpenInputFile(name string) (InputFileReader, error) {
-    filename := fmt.Sprintf("./inputs/%s", name)
-    file, err := os.OpenFile(filename, os.O_RDONLY, 0)
-    if err != nil {
-        return nil, err
-    }
-    ifr := inputFileReader{
-        file:      file,
-        bufreader: nil,
-    }
-    return &ifr, nil
+	filename := fmt.Sprintf("./inputs/%s", name)
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0)
+	if err != nil {
+		return nil, err
+	}
+	ifr := inputFileReader{
+		file:      file,
+		bufreader: nil,
+	}
+	return &ifr, nil
 }
 
 func OpenAndReadAll(name string, callback func(string) error) error {
-    ifr, err := OpenInputFile(name)
-    if err != nil {
-        return err
-    }
-    err = ifr.ReadAll(callback)
-    if err != nil {
-        return err
-    }
-    err = ifr.Close()
-    if err != nil {
-        return err
-    }
-    return nil
+	ifr, err := OpenInputFile(name)
+	if err != nil {
+		return err
+	}
+	err = ifr.ReadAll(callback)
+	if err != nil {
+		return err
+	}
+	err = ifr.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func OpenAndReadAllRegex(name string, regex string, allMustMatch bool) ([][]string, error) {
+	re, err := regexp.Compile(regex)
+	if err != nil {
+		return nil, err
+	}
+	var results [][]string
+	err = OpenAndReadAll(name, func(s string) error {
+		m := re.FindStringSubmatch(s)
+		if m == nil {
+			if allMustMatch {
+				return fmt.Errorf("non-matching line: %s", s)
+			}
+		} else {
+			results = append(results, m)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+func StringsToInts(s []string, positions ...int) ([]int, error) {
+	var results []int
+	for _, p := range positions {
+		v, err := strconv.Atoi(s[p])
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, v)
+	}
+	return results, nil
 }
 
 func (ifr *inputFileReader) Read(p []byte) (n int, err error) {
-    return ifr.bufreader.Read(p)
+	return ifr.bufreader.Read(p)
 }
 
 func (ifr *inputFileReader) Close() error {
-    return ifr.file.Close()
+	return ifr.file.Close()
 }
 
 func (ifr *inputFileReader) ReadLine() (line []byte, isPrefix bool, err error) {
-    if ifr.bufreader == nil {
-        ifr.bufreader = bufio.NewReader(ifr.file)
-    }
-    return ifr.bufreader.ReadLine()
+	if ifr.bufreader == nil {
+		ifr.bufreader = bufio.NewReader(ifr.file)
+	}
+	return ifr.bufreader.ReadLine()
 }
 
 func (ifr *inputFileReader) ReadAll(callback func(string) error) error {
-    scanner := bufio.NewScanner(ifr.file)
-    var err error
-    for scanner.Scan() {
-        err = callback(scanner.Text())
-        if err != nil {
-            break
-        }
-    }
-    if err == nil {
-        err = scanner.Err()
-    }
-    return err
+	scanner := bufio.NewScanner(ifr.file)
+	var err error
+	for scanner.Scan() {
+		err = callback(scanner.Text())
+		if err != nil {
+			break
+		}
+	}
+	if err == nil {
+		err = scanner.Err()
+	}
+	return err
 }
